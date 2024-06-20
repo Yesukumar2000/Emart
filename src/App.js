e6381854f5ffd7567172ade49b743d729ede2ae2
Yesukumar2000 <kumaryesu2000@gmail.com>
2024-06-20 15:29:48 +0530

    add the initial files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         Branch
+        </p>
+        <h1>header one</h1>
+        <p>this is  Branch one</p>
+        <p>this is  Branch Two</p>
+        <p>this is Branch three</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
