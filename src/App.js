41196d7ae1c25dca37d19aa89253769c7a8a7941
Yesukumar2000 <121677945+Yesukumar2000@users.noreply.github.com>
2024-06-20 15:40:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App() {
          Branch
         </p>
         <h1>header one</h1>
-        <p>this is  Branch one</p>
-        <p>this is  Branch Two</p>
-        <p>this is Branch three</p>
+        <h1>Initail change in app.js</h1>    
       </header>
     </div>
   );
